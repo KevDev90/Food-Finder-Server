82061a4a2272dbd179d75fc4e3d12bb690218add
Kevin Johnson <kevinj902302@gmail.com>
2021-05-04 18:03:15 -0400

    comments out old db connection

--- db/index.js
+++ db/index.js
@@ -1,17 +1,15 @@
-const { Pool } = require("pg");
-require("dotenv").config();
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+// const { Pool } = require("pg");
+// require("dotenv").config();
 
-const pool = new Pool(
-  {
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-    ssl: true
-  }
-);
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+// const pool = new Pool(
+//   {
+//     user: process.env.PGUSER,
+//     host: process.env.PGHOST,
+//     database: process.env.PGDATABASE,
+//     password: process.env.PGPASSWORD,
+//     port: process.env.PGPORT,
+//   }
+// );
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+// };
