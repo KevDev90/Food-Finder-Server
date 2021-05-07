a401e81468801a5998068783c2041c7237da739a
Kevin Johnson <kevinj902302@gmail.com>
2021-05-07 15:49:14 -0600

    changes api port connection values

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: 
 const app = express();
 
 const pgdb = knex({
-    client: 'pg',
-    connection: `${process.env.DATABASE_URL}`,
+  client: 'pg',
+  connection: `${process.env.DATABASE_URL}`,
 })
 console.log('processenvurl', process.env.DATABASE_URL)
 app.set('db', pgdb)
@@ -23,138 +23,146 @@ app.use(express.json());
 // }) 
 // Get all Restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
-    console.log('getrequest')
-    try {
-        //const results = await db.query("select * from restaurants");
-        
-        // const restaurantRatingsData = await pgdb.raw(
-        //     "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;"
-        // );
-        const restaurantRatingsData = await pgdb.raw(
-            "select * from restaurants");
-        console.log('dbqueryresults', restaurantRatingsData)
+  console.log('getrequest')
+  try {
+    //const results = await db.query("select * from restaurants");
 
-        res.status(200).json({
-            status: "success",
-            results: restaurantRatingsData.rows.length,
-            data: {
-                restaurants: restaurantRatingsData.rows,
-            },
-        });
-    } catch (err) {
-        console.log(err);
-    }
+    // const restaurantRatingsData = await pgdb.raw(
+    //     "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;"
+    // );
+    const restaurantRatingsData = await pgdb.raw(
+      "select * from restaurants");
+    console.log('dbqueryresults', restaurantRatingsData)
+
+    res.status(200).json({
+      status: "success",
+      results: restaurantRatingsData.rows.length,
+      data: {
+        restaurants: restaurantRatingsData.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //Get a Restaurant
 app.get("/api/v1/restaurants/:id", async (req, res) => {
-    console.log(req.params.id);
+  console.log(req.params.id);
 
-    try {
-        const restaurant = await pgdb.raw(
-            "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1",
-            [req.params.id]
-        );
-        // select * from restaurants wehre id = req.params.id
+  try {
+    const restaurant = await pgdb.raw(
+      "select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1",
+      [req.params.id]
+    );
+    // select * from restaurants wehre id = req.params.id
 
-        const reviews = await pgdb.raw(
-            "select * from reviews where restaurant_id = $1",
-            [req.params.id]
-        );
-        console.log(reviews);
+    const reviews = await pgdb.raw(
+      "select * from reviews where restaurant_id = $1",
+      [req.params.id]
+    );
+    console.log(reviews);
 
-        res.status(200).json({
-            status: "succes",
-            data: {
-                restaurant: restaurant.rows[0],
-                reviews: reviews.rows,
-            },
-        });
-    } catch (err) {
-        console.log(err);
-    }
+    res.status(200).json({
+      status: "succes",
+      data: {
+        restaurant: restaurant.rows[0],
+        reviews: reviews.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Create a Restaurant
 
 app.post("/api/v1/restaurants", async (req, res) => {
-    console.log('postreq', req.body);
+  console.log('postreq', req.body);
 
-    try {
-        const results = await pgdb.raw(
-            "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
-            [req.body.name, req.body.location, req.body.price_range]
-        );
-        console.log('dbpostqueryresults', results)
-        
-        res.status(201).json({
-            status: "succes",
-            data: {
-                restaurant: results.rows[0],
-            },
-        });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const results = await pgdb.raw(
+      "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
+      [req.body.name, req.body.location, req.body.price_range]
+    );
     console.log('dbpostqueryresults', results)
+
+    res.status(201).json({
+      status: "succes",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  console.log('dbpostqueryresults', results)
 });
 
 // Update Restaurants
 
 app.put("/api/v1/restaurants/:id", async (req, res) => {
-    try {
-        const results = await pgdb.raw(
-            "UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
-            [req.body.name, req.body.location, req.body.price_range, req.params.id]
-        );
+  try {
+    const results = await pgdb.raw(
+      "UPDATE restaurants SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    );
 
-        res.status(200).json({
-            status: "succes",
-            data: {
-                retaurant: results.rows[0],
-            },
-        });
-    } catch (err) {
-        console.log(err);
-    }
-    console.log(req.params.id);
-    console.log(req.body);
+    res.status(200).json({
+      status: "succes",
+      data: {
+        retaurant: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(req.params.id);
+  console.log(req.body);
 });
 
 // Delete Restaurant
 
 app.delete("/api/v1/restaurants/:id", async (req, res) => {
-    try {
-        const results = pgdb.raw("DELETE FROM restaurants where id = $1", [
-            req.params.id,
-        ]);
-        res.status(204).json({
-            status: "sucess",
-        });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const results = pgdb.raw("DELETE FROM restaurants where id = $1", [
+      req.params.id,
+    ]);
+    res.status(204).json({
+      status: "sucess",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
-    try {
-        const newReview = await pgdb.raw(
-            "INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *;",
-            [req.params.id, req.body.name, req.body.review, req.body.rating]
-        );
-        console.log(newReview);
-        res.status(201).json({
-            status: "success",
-            data: {
-                review: newReview.rows[0],
-            },
-        });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const newReview = await pgdb.raw(
+      "INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *;",
+      [req.params.id, req.body.name, req.body.review, req.body.rating]
+    );
+    console.log(newReview);
+    res.status(201).json({
+      status: "success",
+      data: {
+        review: newReview.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 const port = process.env.PORT || 3002;
-app.listen(3001, () => {
+const { NODE_ENV, PORT, IP } = process.env;
+
+if (NODE_ENV === "production") {
+  app.listen(PORT, IP, () => {
+    console.log(`server is up and listening on ${IP}:${PORT}`);
+  });
+} else {
+  app.listen(3001, () => {
     console.log(`server is up and listening on port 3001`);
-});
+  });
+}
