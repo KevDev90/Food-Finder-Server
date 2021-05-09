70832f638c2d22bf2d198003175288e0514e6c47
Kevin Johnson <kevinj902302@gmail.com>
2021-05-08 20:48:29 -0600

    alters config

--- config/config.js
+++ config/config.js
@@ -23,10 +23,9 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    ssl: true,
     dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false
-      }
+      ssl: true
     },
   }
 }
