53f772c54645a877a7c424bc4a67955a7a83a80b
Kevin Johnson <kevinj902302@gmail.com>
2021-05-08 20:37:08 -0600

    formats env variable

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_environment_variable: 'DATABASE_URL',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'
   }
 }
