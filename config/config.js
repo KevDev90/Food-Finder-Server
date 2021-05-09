3e63a2054704834975b7f4643a1400114f07b513
Kevin Johnson <kevinj902302@gmail.com>
2021-05-08 20:41:54 -0600

    adds reject-unauthorized-ssl to production connection

--- config/config.js
+++ config/config.js
@@ -22,6 +22,11 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
   }
 }
