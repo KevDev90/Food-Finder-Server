b9d6b6f6a3603eb8a015f09c73553e9283d1be81
Kevin Johnson <kevinj902302@gmail.com>
2021-05-21 16:23:17 +0500

    Alters sequelize connection

--- config/config.js
+++ config/config.js
@@ -25,7 +25,7 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    ssl: true,
+    // ssl: true,
     dialectOptions: {
       ssl: {
         require: true,
