f19e83165823a012065fb837482f3596f5cbda99
Kevin Johnson <kevinj902302@gmail.com>
2021-05-21 16:27:49 +0500

    Adds config obj to sequelize connection

--- utils/dbconnection.util.js
+++ utils/dbconnection.util.js
@@ -7,7 +7,7 @@ const env = process.env.NODE_ENV || 'development'
 const config = configs[env];
 export let sequelize;
 if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], process.env[config]);
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
